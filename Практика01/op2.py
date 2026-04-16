28a5ce1017cf2d9903940799034ae57b53ade42d
Ismail <mobaisa01@gmail.com>
2026-04-16 17:41:44 +0600

    level-01

---
+++ Практика01/op2.py
@@ -0,0 +1,45 @@
+class SmartDevice:
+    def init(self, brand, model, battery_level, is_on):
+        self.brand = brand
+        self.model = model
+        self.__battery_level = battery_level
+        self.__is_on = is_on
+
+
+    def turn_on(self):
+        self.__is_on = True
+
+
+    def turn_off(self):
+        self.__is_on = False
+
+
+    def charge(self, amount):
+        self.__battery_level += amount
+
+
+    def use(self, minutes):
+        self.__battery_level -= minutes
+
+
+    def get_status(self):
+        status = "Да" if self.__is_on else "Нет"
+        return f"{self.brand} {self.model}  . Включено: {status} .  Заряд: {self.__battery_level}%"
+
+
+
+
+device = SmartDevice()
+device.init("Samsung", "011", 50, True)
+
+print(device.get_status())
+
+device.use(50)
+device.charge(0)
+device.turn_off()
+
+print(device.get_status())
+
+
+
+
